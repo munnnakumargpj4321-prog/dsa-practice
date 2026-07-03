04787e5178cac740265d7fefa701a972ee586686
Munna Kumar <munnnakumargpj4321@gmail.com>
2026-07-03 17:05:12 +0530

    Time: 2 ms (19.62%), Space: 8.7 MB (13.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 0) return -1;   
+        if (x == 0 || x == 1) return x;
+
+        long long left = 1, right = x, ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                ans = mid;     
+                left = mid + 1; 
+            } else { 
+                right = mid - 1; 
+            }
+        }
+        return (int)ans;
+    }
+};
