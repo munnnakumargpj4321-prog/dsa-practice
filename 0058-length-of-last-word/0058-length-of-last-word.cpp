1706abfe23c7b61c2765bfd60cf187d655afebb2
Munna Kumar <munnnakumargpj4321@gmail.com>
2026-07-05 13:52:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.98%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int l=s.length()-1;
+        int count=0;
+        while(l>=0&&s[l]==' '){
+            l--;
+        }
+        while(l>=0&&s[l]!=' '){
+            l--;
+            count++;
+        }
+        
+        return count;
+    }
+};
