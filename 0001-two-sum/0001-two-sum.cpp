0267e29f38b0df9800a29d52ea880fb72a84122e
Munna Kumar <munnnakumargpj4321@gmail.com>
2026-07-23 12:36:36 +0530

    Time: 3 ms (66.51%), Space: 14.9 MB (38.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,26 +3,61 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        int i=0;
-        int j=n-1;
-        bool found=false;
+        unordered_map<int,int>m;
         vector<int>ans;
         for(int  i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    found=true;
-                    break;
-                }
-            }
-            if(found==true){
+            int first=nums[i];
+            int second=target-first;
+            if(m.find(second)!=m.end()){
+                ans.push_back(i);
+                ans.push_back(m[second]);
                 break;
             }
+            m[first]=i;
+           
+          
         }
         return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         // int i=0;
+//         // int j=n-1;
+//         bool found=false;
+//         vector<int>ans;
+//         for(int  i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     ans.push_back(i);
+//                     ans.push_back(j);
+//                     found=true;
+//                     break;
+//                 }
+//             }
+//             if(found==true){
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
 //  // this is going  to  be solve with the harse method 
 // class Solution {
 // public:
